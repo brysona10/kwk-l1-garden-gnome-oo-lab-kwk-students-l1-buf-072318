bc4a9cf6f25acf27cbf594a22c90eaaad159f5fa
brysona10 <addisonbryson10@gmail.com>
2018-07-30 14:09:31 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -3,7 +3,7 @@ class GardenGnome
   attr_accessor :name, :age, :gluten_allergy, :personality, :hat_color
   def initialize(hat_color = "red")
     @personality = "evil"
-    @hat_color = :hat_color
+    @hat_color = +:hat_color
   end
 end 
 
