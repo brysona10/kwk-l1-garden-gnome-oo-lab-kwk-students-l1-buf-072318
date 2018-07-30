8cb7939189df2c8bb54c770cb5681d93dbb3964c
brysona10 <addisonbryson10@gmail.com>
2018-07-30 14:30:34 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -12,7 +12,7 @@ class GardenGnome
     "GNARLY!!!"
   end
   def introduce_self
-    Hello humans, my name is Frankie the Frightening, I am 324 years old, and you'll rue the day you crossed me!"
+    "Hello humans, my name is Frankie the Frightening, I am 324 years old, and you'll rue the day you crossed me!"
 end 
 
 
